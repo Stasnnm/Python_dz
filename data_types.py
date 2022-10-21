8d43a79d4ac8193694ebbfd53d359b4186f2ad66
User <nnm86@ukr.net>
2022-10-21 14:49:36 +0300

    homework number one

---
+++ data_types.py
@@ -0,0 +1,24 @@
+student_name = 'Vasiliy Vasilyev'
+student_country = 'Ukrain'
+students_city = 'Odessa'
+student_street = 'Pushkinskaya'
+house_nomber = 15
+flat_number = 7
+date_of_birthday_year = 1990
+date_of_birthday_month = 'june'
+date_of_birthday_day = 10
+
+student_age = 20
+telephone_number = 75102369
+print('name of student:', student_name, type(student_name))
+print('adress:', student_country, type(student_country))
+print('city', students_city, type(students_city))
+print('street:', student_street, type(student_street))
+print('house:', house_nomber, type(house_nomber))
+print('flat:', flat_number, type(flat_number))
+print('day of birthday:', date_of_birthday_day, type(date_of_birthday_day))
+print('birthday month:', date_of_birthday_month, type(date_of_birthday_month))
+print('yaer:', date_of_birthday_year, type(date_of_birthday_year))
+print('age:', student_age, type(student_age))
+print('telephone:', telephone_number, type(telephone_number))
+
