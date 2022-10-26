ff278a1dcf2a4205a93c32239cc345ab3e868f66
User <nnm86@ukr.net>
2022-10-26 14:27:45 +0300

    dz 3

--- bool_type.py
+++ bool_type.py
@@ -14,26 +14,28 @@ print(not True or not True or not False)
 print(not True or True and not False)
 
 bool_none = bool(None)
-print('bool_none -', bool_none)
 bool_numb = bool (7)
-print('bool_numb -', bool_numb)
-comp = bool(bool_numb == bool_none)
+print('bool_none -', bool_none, 'bool_numb -', bool_numb)
+comp = bool(bool_numb) == bool(bool_none)
 print('bool_none == bool_numb -', comp)
+
 empt_str = bool("")
 subtr = bool(10 - 1)
 print('empt_str', empt_str, 'subtr', subtr)
-comp_two = bool(empt_str == subtr)
+comp_two = bool(empt_str) == bool(subtr)
 print('bool("") == bool(10 - 1) -', comp_two)
+
 tru_or_fal = bool(True or False)
-f_prin = print('123')
-comp_three = bool(tru_or_fal) == f_prin
+f_prin = bool(print('123'))
 print('bool True or False -', tru_or_fal)
 print('bool(print("123"))- ', f_prin)
-print('bool(True or false) == bool(print("123"))-', tru_or_fal == f_prin)
+comp_three = bool(tru_or_fal) == bool(f_prin)
+print('bool(True or false) == bool(print("123"))-', comp_three)
+
 f_typ = bool(type(None))
 f_id = bool(id(None))
 print('bool(type(None)) -', f_typ, 'bool(id(None))', f_id)
-comp_four = print('bool(type(None)) == bool(id(None))', f_typ == f_id)
+comp_four = print('bool(type(None)) == bool(id(None))', bool(f_typ) == bool(f_id))
 
 
 
