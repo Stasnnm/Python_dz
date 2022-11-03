2f128729d832f0a355704d361ce07fdf7ec299f9
User <nnm86@ukr.net>
2022-11-03 11:19:46 +0300

    dz 3.2

--- rows_and_numbers.py
+++ rows_and_numbers.py
@@ -1,6 +1,5 @@
 name = input('input your name:')
 print(f'Good day {name}')
-
 val = float(input('input float number: '))
 print('your value is :', val)
 val_int = int(val)
@@ -9,3 +8,9 @@ val_deg = val_int ** 4
 print('your value in fourth degree', val_deg)
 val_squar = val_int ** 0.5
 print('square of your number', val_squar)
+div_val = (val_int % 2)
+print('remainder of division :', div_val)
+
+
+
+
