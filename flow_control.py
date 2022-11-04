ea24fd2828ca96697c01538edd191bd9f27f9ae9
User <nnm86@ukr.net>
2022-11-04 15:55:37 +0300

    dz 3.2

--- flow_control.py
+++ flow_control.py
@@ -1,21 +1,19 @@
-num_one = float(input('input number one :'))
-num_two = float(input('input nomber two :'))
-ope_num = input('input operation "+" or "-" or "/" or "*"')
+num_one = float(input('input number one : '))
+num_two = float(input('input nomber two : '))
+ope_num = input('input operation "+" or "-" or "/" or "*" : ')
 if ope_num == '+' :
     res = num_one + num_two
-    print('result', res)
 elif ope_num == '-':
     res = num_one - num_two
-    print('result', res)
 elif ope_num == '/':
     res = num_one / num_two
-    print('result', res)
 elif ope_num == '*':
     res = num_one * num_two
-    print('result', res)
 else :
     print('incorrect operation')
-
-
+if  num_one - int(num_one) > 0 and num_two - int(num_two) > 0:
+    print(float(res))
+else:
+    print(int(res))
 
 
