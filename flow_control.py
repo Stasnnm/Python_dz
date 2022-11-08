1cdefeaa62028584a3ec7953600d45b9680e4ae2
User <nnm86@ukr.net>
2022-11-08 15:26:16 +0300

    1

--- flow_control.py
+++ flow_control.py
@@ -1,17 +1,17 @@
 num_one = input('input number one : ')
 num_two = input('input nomber two : ')
-try:
-    num_one = int(num_one)
-    print('number one is integer')
-except:
+if '.' in num_one:
     num_one = float(num_one)
     print('number one is float')
-try:
-    num_two = int(num_two)
-    print('number two is integer')
-except:
+else:
+    num_one = int(num_one)
+    print('number one is integer')
+if '.' in num_two:
     num_two = float(num_two)
     print('number two is float')
+else:
+    num_two = int(num_two)
+    print('number two is integer')
 ope_num = input('input operation "+" or "-" or "/" or "*" or "**" : ')
 if ope_num == '+':
     res = num_one + num_two
@@ -25,6 +25,7 @@ elif ope_num == '**':
     res = num_one ** num_two
 else:
     print('incorrect operation')
+print('result :', res)
 print('type of result is: ', type(res))
 if num_one < num_two:
     print('num_one > num_two')
@@ -32,13 +33,13 @@ elif num_one > num_two:
     print('num_one > num_two')
 else:
     print('num_one = num_two')
-if (num_one % 1) == 0:
-        print('order num_one :', len(str(int(num_one))))
+if type(num_one) is int:
+   print('order num_one :', len(str(num_one)))
 else:
     len_fl = len(str(num_one))
     print('order num_one:', len_fl - 1)
-if (num_two % 1) == 0:
-    print('order num_two :', len(str(int(num_two))))
+if type(num_two) is int:
+    print('order num_two :', len(str(num_two)))
 else:
     len_fl_two = len(str(num_two))
     print('order num_two:', len_fl_two - 1)
